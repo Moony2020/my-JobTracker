d96005ca29f470e62fff2dd0ea83d8d33c759cdd
Mymoon Dobaibi <mymoon676@hotmail.com>
2026-01-16 02:56:43 +0100

    feat: Implement initial JobTracker web application with `index.html` for the frontend and `server.js` for the backend.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,8 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 10000,
   })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
